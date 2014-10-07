f700af5ff5bd6a98ce9e5c4e30ef25e4a1163e6d
Bjorn Roberg <bjorn.roberg@gmail.com>
2014-10-07 14:55:25 +0200

    use free font

--- graphical-interface.el
+++ graphical-interface.el
@@ -1,6 +1,6 @@
 ;;; GUI initialization
 
-(set-face-attribute 'default nil :height 110 :font "Consolas")
+(set-face-attribute 'default nil :height 110 :font "DejaVuSansMono")
 
 (setq inhibit-startup-message t) ;; no splash screen
 
