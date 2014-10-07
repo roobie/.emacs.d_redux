bc1c047051c62ef7b65ed69a26b30473ee453cd8
Bjorn Roberg <bjorn.roberg@gmail.com>
2014-10-07 14:55:08 +0200

    Update key bindings to work with new code from yasnippet

--- key-bindings.el
+++ key-bindings.el
@@ -3,7 +3,10 @@
 (define-key global-map (kbd "RET") 'newline-and-indent)
 (define-key global-map (kbd "M-RET") 'newline)
 
-(define-key global-map (kbd "M-e") 'yas/expand)
+;(define-key global-map (kbd "M-e") 'yas/expand)
+(define-key yas-minor-mode-map (kbd "<tab>") nil)
+(define-key yas-minor-mode-map (kbd "TAB") nil)
+(define-key yas-minor-mode-map (kbd "M-e") 'yas-expand)
 
 (define-key global-map (kbd "C-<tab>") 'other-window)
 
